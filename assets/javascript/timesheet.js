4d72369aa07cb1ddbc75fea236696d1effc69c97
Michael Rosario <michael@elementoinc.com>
2019-02-23 12:43:41 -0500

    Update styles and html

--- assets/javascript/timesheet.js
+++ assets/javascript/timesheet.js
@@ -14,64 +14,79 @@ var config = {
 
   $("button[type=submit]").on("click", function (event) {
 
-    event.preventDefault();
+    event.preventDefault(); // avoid reloading of page
 
+    // get employee name
     var employeeName = $("#employeeName").val().trim();
-    var employeeRole = $("#employeeRole").val().trim();
-    var employeeStartDate = $("#employeeStartDate").val().trim();
-    var employeeMonthlyRate = $("#employeeMonthlyRate").val().trim();
 
-    database.ref().push({
-      employeeName: employeeName,
-      employeeRole: employeeRole,
-      employeeStartDate: employeeStartDate,
-      employeeMonthlyRate: employeeMonthlyRate
-    });
+    // make sure employee exists
+    if(employeeName){
+        var employeeRole = $("#employeeRole").val().trim();
+        var employeeStartDate = $("#employeeStartDate").val().trim();
+        var employeeMonthlyRate = $("#employeeMonthlyRate").val().trim();
+        
+        // push to database
+        database.ref().push({
+          employeeName: employeeName,
+          employeeRole: employeeRole,
+          employeeStartDate: employeeStartDate,
+          employeeMonthlyRate: employeeMonthlyRate
+        });
+    }
+   
   });
 
+  // watch changes and update 
   database.ref().orderByChild("employeeStartDate").on("child_added",function(snapshot){
 
     var entry = snapshot.val();
-
     var today = new Date();
-    var employeeMonths = Math.round(moment(today).diff(moment(entry.employeeStartDate), 'months', true));
     
-    var rowEntry = $("<tr>");
-    var rowCol = $("<td>");
-    rowCol.addClass("col");
-    rowCol.text(entry.employeeName);
-    rowEntry.append(rowCol);
+    // make sure entry exists
+    if(entry.employeeName){
 
-    rowCol = $("<td>");
-    rowCol.addClass("col");
-    rowCol.text(entry.employeeRole);
-    rowEntry.append(rowCol);
-
-    rowCol = $("<td>");
-    rowCol.addClass("col");
-    rowCol.text(entry.employeeStartDate);
-    rowEntry.append(rowCol);
-
-    rowCol = $("<td>");
-    rowCol.addClass("col");
-    rowCol.text(employeeMonths);
-    rowEntry.append(rowCol);
-
-    rowCol = $("<td>");
-    rowCol.addClass("col rate");
-    rowCol.text(entry.employeeMonthlyRate);
-    rowEntry.append(rowCol);
+        var employeeMonths = Math.round(moment(today).diff(moment(entry.employeeStartDate), 'months', true));
     
-    var total = entry.employeeMonthlyRate * employeeMonths;
-    rowCol = $("<td>");
-    rowCol.addClass("col total");
-    rowCol.text(total);
-    rowEntry.append(rowCol);
+        var rowEntry = $("<tr>");
+        var rowCol = $("<td>");
+        rowCol.addClass("col");
+        rowCol.text(entry.employeeName);
+        rowEntry.append(rowCol);
+    
+        rowCol = $("<td>");
+        rowCol.addClass("col");
+        rowCol.text(entry.employeeRole);
+        rowEntry.append(rowCol);
+    
+        rowCol = $("<td>");
+        rowCol.addClass("col");
+        rowCol.text(entry.employeeStartDate);
+        rowEntry.append(rowCol);
+    
+        rowCol = $("<td>");
+        rowCol.addClass("col");
+        rowCol.text(employeeMonths);
+        rowEntry.append(rowCol);
+    
+        rowCol = $("<td>");
+        rowCol.addClass("col rate");
+        rowCol.text(entry.employeeMonthlyRate);
+        rowEntry.append(rowCol);
+        
+        var total = entry.employeeMonthlyRate * employeeMonths;
+        rowCol = $("<td>");
+        rowCol.addClass("col total");
+        rowCol.text(total);
+        rowEntry.append(rowCol);
+    
+        console.log(rowEntry);
+    
+        $('tbody').append(rowEntry);
+        $(".total,.rate").formatCurrency({ roundToDecimalPlace: 2, eventOnDecimalsEntered: true });
 
-    console.log(rowEntry);
+    }
 
-    $('tbody').append(rowEntry);
-    $(".total,.rate").formatCurrency({ roundToDecimalPlace: 2, eventOnDecimalsEntered: true });
+   
 
   });
 
